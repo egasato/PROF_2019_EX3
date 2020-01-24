40d7d1e7697e93a9233362396310831359aff331
Esaú García Sánchez-Torija <egasato@protonmail.com>
2020-01-24 14:49:01 +0000

    Refactorizada la clase MyClass para inyectar dependencia Time

--- src/main/java/es/upm/grise/profundizacion2019/ex3/MyClass.java
+++ src/main/java/es/upm/grise/profundizacion2019/ex3/MyClass.java
@@ -2,9 +2,17 @@ package es.upm.grise.profundizacion2019.ex3;
 
 public final class MyClass {
 
+	// La instancia usada para calcular el tiempo
+	private Time time;
+
+	// Un constructor que inyecta la dependencia
+	public MyClass(final Time time) {
+		this.time = time;
+	}
+
 	// Calcula un tiempo futuro usando el tiempo actual como base
 	public void nextTime(final long seconds) {
-		final String next = new Time().getFutureTime(seconds);
+		final String next = time.getFutureTime(seconds);
 		System.out.println(next);
 	}
 
