13b0234f8b210a5e3f901e100674aa70dc58bed4
Esaú García Sánchez-Torija <egasato@protonmail.com>
2020-01-24 14:32:55 +0000

    Añadido test del ejercicio 2 (clase Time)

--- src/test/java/es/upm/grise/profundizacion2019/ex3/smokeTest.java
+++ src/test/java/es/upm/grise/profundizacion2019/ex3/smokeTest.java
@@ -1,13 +1,17 @@
 package es.upm.grise.profundizacion2019.ex3;
 
-import static org.junit.Assert.*;
+import java.time.LocalDateTime;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class smokeTest {
+
+	// La instancia de la clase MyClass a testear
 	MyClass my;
-	
+
 	@Before
 	public void setUp() {
 		my = new MyClass();
@@ -18,4 +22,14 @@ public class smokeTest {
 		long SECONDS = 60;
 		my.nextTime(SECONDS);
 	}
+
+	@Test
+	public void pruebaDeEstadoTime() {
+		final LocalDateTime tiempoActual       = LocalDateTime.of(2020, 1, 1, 0, 0, 0);
+		final long          diferenciaDeTiempo = 120;
+		final String        tiempoFuturo       = "2020/01/01 00:02:00";
+		final Time time = new Time(tiempoActual);
+		assertEquals(tiempoFuturo, time.getFutureTime(diferenciaDeTiempo));
+	}
+	
 }
