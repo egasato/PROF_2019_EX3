ac75ad5522f781c0607bfa988210d35b4654813b
Esaú García Sánchez-Torija <egasato@protonmail.com>
2020-01-24 14:31:06 +0000

    Refactorizar clase Time para inyectar dependencias

--- src/main/java/es/upm/grise/profundizacion2019/ex3/Time.java
+++ src/main/java/es/upm/grise/profundizacion2019/ex3/Time.java
@@ -3,13 +3,30 @@ package es.upm.grise.profundizacion2019.ex3;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class Time {
+public final class Time {
+
+	// El formato del tiempo y fecha es constante
+	private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 	
-	public static String getFutureTime(long seconds) {
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
-		LocalDateTime time = LocalDateTime.now();
-		time.plusSeconds(seconds);
-		return formatter.format(time);	
+	// La instancia usada para calcular la diferencia de tiempo
+	private LocalDateTime time;
+	
+	// Un constructor que inyecta la dependencia
+	public Time(final LocalDateTime time) {
+		this.time = time;
+	}
+
+	// Un constructor vacío utiliza el tiempo actual por defecto
+	public Time() {
+		this(LocalDateTime.now());
+	}
+
+	// Un método que computa un tiempo futuro
+	// PD: Teoricamente la clase LocalDateTime es inmutable, por lo que he
+	//     asumo que hay que asignar el objeto retornado por "plusSeconds"
+	public String getFutureTime(final long seconds) {
+		final LocalDateTime future = time.plusSeconds(seconds);
+		return formatter.format(future);	
 	}
 
 }
